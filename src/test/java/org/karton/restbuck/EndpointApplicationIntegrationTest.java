558007aa7bb561de368f6e3fae1507fe44e362e8
pkan <piotr.kantor@impaqgroup.com>
2016-01-11 09:41:31 +0100

    Adding test that check users add and get endpoints without lounching the server. It uses only Spring context so should be faster.

--- src/test/java/org/karton/restbuck/EndpointApplicationIntegrationTest.java
+++ src/test/java/org/karton/restbuck/EndpointApplicationIntegrationTest.java
@@ -1,51 +1,41 @@
 package org.karton.restbuck;
 
+
 import com.karton.restbuck.EndpointApplication;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = EndpointApplication.class)
+@IntegrationTest
 @WebAppConfiguration
-public class EndpointApplicationTest {
+public class EndpointApplicationIntegrationTest {
 
     final String NAME ="highlander";
     final String GET="http://localhost:8080/users/get";
     final String ADD="http://localhost:8080/users/add?name="+ NAME;
 
-    @Autowired
-    WebApplicationContext webCtx;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-        mockMvc= MockMvcBuilders.webAppContextSetup(webCtx).build();
-    }
-
     @Test
-    public void shouldGetCorrectUser() throws Exception {
-        mockMvc.perform(get(ADD));
-        MvcResult result= mockMvc.perform(get(GET)).andReturn();
-        String actual=result.getResponse().getContentAsString();
+    public void testGetUser() throws Exception{
+        new RestTemplate().getForObject(ADD, String.class);
+        String result=new RestTemplate().getForObject(GET, String.class);
         String expected=new StringBuilder()
                 .append("[{\"name\":\"")
                 .append(NAME)
                 .append("\"}]")
                 .toString();
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected, result);
     }
 
 }
