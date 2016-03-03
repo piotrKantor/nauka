267e627b6758330133c4512ecddf10d7b5cba586
Piotr Kantor <piotr.kantor@gmail.com>
2016-03-03 11:08:33 +0100

    Changing config to prevent generated login endpoint from returning html if authentication fails and error when authentication succeed. Fixing shouldGetTheSameUserNameItPosted() test so that restTemplate keeps session after login

--- src/main/java/com/karton/restbuck/config/WebSecurityConfig.java
+++ src/main/java/com/karton/restbuck/config/WebSecurityConfig.java
@@ -4,15 +4,26 @@ import com.karton.restbuck.user.UserAccountDetailsService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.Http401AuthenticationEntryPoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -27,13 +38,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/users").hasAuthority("USER")
                 .and()
                 .formLogin().loginProcessingUrl("/login")
-                .permitAll();
+                .successHandler(successHandler())
+                .permitAll()
+                .defaultSuccessUrl("/users")
+                .and()
+                .exceptionHandling().accessDeniedPage("/403")
+                .authenticationEntryPoint(new Http401AuthenticationEntryPoint("401"));
+    }
+
+    /**
+     * This method
+     */
+    private AuthenticationSuccessHandler successHandler() {
+        return new SimpleUrlAuthenticationSuccessHandler(){
+
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+                    throws IOException, ServletException {
+                clearAuthenticationAttributes(request);
+            }
+        };
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth, UserAccountDetailsService userAccountDetailsService) throws Exception {
-        auth.userDetailsService(userAccountDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder())
-        ;
+        auth.userDetailsService(userAccountDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
+
 }
--- src/test/java/org/karton/restbuck/user/web/UserAccountControllerIntegrationTest.java
+++ src/test/java/org/karton/restbuck/user/web/UserAccountControllerIntegrationTest.java
@@ -4,14 +4,12 @@ package org.karton.restbuck.user.web;
 import com.karton.restbuck.Main;
 import com.karton.restbuck.user.UserAccount;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
@@ -24,22 +22,31 @@ import java.util.List;
 @WebAppConfiguration
 public class UserAccountControllerIntegrationTest {
 
+    private final String BASE ="http://localhost:8080/";
     private final String NAME ="highlander";
     private final String PASSWORD ="&password=there_can_be_only_one";
-    private final String GET="http://localhost:8080/users";
-    private final String ADD="http://localhost:8080/users?name=";
-    private final String LOGIN="http://localhost:8080/login?username=highlander&password=there_can_be_only_one";
+    private final String USERS="users";
+    private final String ADD="?name=";
+    private final String LOGIN="login?username=";
 
-    @Ignore //TODO Refactor this and other tests in next commit
     @Test
-    public void shoulgGetTheSameUserNameItPosted() throws Exception{
+    public void shouldGetTheSameUserNameItPosted() throws Exception{
+        //given
         RestTemplate restTemplate=new RestTemplate();
-        restTemplate.postForLocation(ADD+NAME+PASSWORD, String.class);
-        restTemplate.postForLocation(LOGIN, String.class);
+        restTemplate.postForLocation(BASE+ USERS +ADD+NAME+PASSWORD, String.class);
+        ResponseEntity responseEntity=restTemplate.postForEntity(BASE+LOGIN+NAME+PASSWORD, null, String.class);
+        String cookie=responseEntity.getHeaders().get("Set-Cookie").get(0);
+        HttpHeaders requestHeaders=new HttpHeaders();
+        requestHeaders.add("Cookie", cookie);
+        HttpEntity requestEntity=new HttpEntity(requestHeaders);
+
+        //when
         ResponseEntity<List<UserAccount>> usersResponse =
-                restTemplate.exchange(GET, HttpMethod.GET, null, new ParameterizedTypeReference<List<UserAccount>>() {});
+                restTemplate.exchange(BASE+ USERS, HttpMethod.GET, requestEntity, new ParameterizedTypeReference<List<UserAccount>>() {});
         List<UserAccount> userAccounts = usersResponse.getBody();
         String actual= userAccounts.get(0).getName();
+
+        //then
         Assert.assertEquals(NAME, actual);
     }
 
